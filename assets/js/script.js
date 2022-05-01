79f9b5d5e6bb01c9400f8224f01e531309586556
Josh-Wickens <joshwickens@hotmail.com>
2022-05-01 14:56:48 +0000

    add function to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+function runGame() {
+
+}
+
+function howToPlay() {
+
+}
+
+function countCards() {
+
+}
+
+function hit() {
+
+}
+
+function stick() {
+
+}
+
+function bust() {
+
+}
+
+function winRound() {
+
+}
+
+function loseRound() {
+
+}
+
+function drawRound() {
+
+}
+
+function increaseScore() {
+
+}
+
+function loseGame() {
+
+}
+
+function winGame() {
+
+}
