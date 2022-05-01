a2566e5e00d98214de97c719f6f744afaa528d9d
Josh-Wickens <joshwickens@hotmail.com>
2022-05-01 18:47:45 +0000

    generate random number for player and computer card. comment out create name sections until game logic is sorted.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,35 @@
 //* wait for DOM to load/*
 //* get the button tags and add event listeners to them/*
 
+/*  commented out code whilst sort game logic!!
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByTagName("input")[0].focus();
     insertName();
 });
 
+*/
 
 function insertName() {
-
-
 
 }
 
 
 function runGame() {
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
 
+}
+
+runGame();
+
+function displayCardNumber() {
+    document.getElementById("computer-card-number").innerHTML = num1;
+    document.getElementById("player-card-number").innerHTML = num2;
+
+}
+
+{
 
 }
 
